2974b9d0085c7e38d885fdb0fdbe061dc96926ae
zenenperaza <peraza@outlook.com>
2023-06-07 09:58:31 -0400

    secondç commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> 
+    <router-link to="/">Home</router-link>  |  
+    <router-link to="/auth">Access</router-link> 
   </nav>
   <router-view/>
 </template>
---
+++ src/views/AuthView.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="container"> 
+    <h1>Auth</h1>
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model="password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+
+</form>
+</div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from 'vue'
+
+   let email = ref("")
+   let password = ref("")
+</script>
+
+<style scoped>
+
+</style>
