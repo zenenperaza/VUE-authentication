463b26e237eb87f7037405a3024d19d6dbdda7e3
zenenperaza <peraza@outlook.com>
2023-06-08 21:39:21 -0400

    commit 2

--- src/views/SocialView.vue
+++ src/views/SocialView.vue
@@ -3,16 +3,18 @@
     <h1>Social Login</h1>
     <button class="btn btn-info  m-3 p-3 " style="width: 25%" v-on:click="loginGoogle">Login with Google</button>
     <button class="btn btn-info  m-3 p-3 " style="width: 25%" v-on:click="loginFacebook">Login with Facebook</button>
-    <button class="btn btn-info  m-3 p-3 " style="width: 25%">Login with Twitter</button>
-    <button class="btn btn-info  m-3 p-3 " style="width: 25%">Login with Gitub</button>
+    <button class="btn btn-info  m-3 p-3 " style="width: 25%" v-on:click="loginTwitter">Login with Twitter</button>
+    <button class="btn btn-info  m-3 p-3 " style="width: 25%" v-on:click="loginGithub">Login with Gitub</button>
 </div>
 </template>
 
 <script lang="ts" setup>
-import { GoogleAuthProvider, signInWithPopup, getAuth, FacebookAuthProvider } from "firebase/auth"
+import { GoogleAuthProvider, signInWithPopup, getAuth, FacebookAuthProvider, TwitterAuthProvider, GithubAuthProvider } from "firebase/auth"
 
 const googleProvider = new GoogleAuthProvider()
 const facebookProvider = new FacebookAuthProvider()
+const twitterProvider = new TwitterAuthProvider()
+const githubProvider = new GithubAuthProvider()
 
 const auth = getAuth()
 
@@ -28,10 +30,34 @@ const loginGoogle = () => {
     })
 }
 
+
+const loginGithub = () => {
+    signInWithPopup(auth, githubProvider)
+    .then((result) => {
+        const credential = GithubAuthProvider.credentialFromResult(result)
+        const token = credential?.accessToken
+        alert(token)
+    })
+    .catch((error) => {
+        alert("Loig failed!")
+    })
+}
 const loginFacebook = () => {
     signInWithPopup(auth, facebookProvider)
     .then((result) => {
         const credential = FacebookAuthProvider.credentialFromResult(result)
+        const token = credential?.accessToken
+        alert(token)
+    })
+    .catch((error) => {
+        alert("Loig failed!")
+    })
+}
+
+const loginTwitter= () => {
+    signInWithPopup(auth, twitterProvider)
+    .then((result) => {
+        const credential = TwitterAuthProvider.credentialFromResult(result)
         const token = credential?.accessToken
         alert(token)
     })
